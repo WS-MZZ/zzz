d4d8e87563a332db7e88982f6ef39ae02977c390
haomingdave <291053702@qq.com>
2020-09-06 15:22:15 +0800

    更新

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -33,7 +33,7 @@ export default {
       const first = matched[0]
 
       if (!this.isDashboard(first)) {
-        matched = [{ path: '/home', meta: { title: '企业管理后台' }}].concat(matched)
+        matched = [{ path: '/home', meta: { title: 'sass管理后台' }}].concat(matched)
       }
 
       this.levelList = matched.filter(item => item.meta && item.meta.title && item.meta.breadcrumb !== false)
