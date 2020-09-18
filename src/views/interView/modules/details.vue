8a84091842ac67e3553193b5dc76d68121305a0f
haomingdave <291053702@qq.com>
2020-09-18 23:02:21 +0800

    触发

--- src/views/interView/modules/details.vue
+++ src/views/interView/modules/details.vue
@@ -17,7 +17,7 @@
                 <span>{{ form.resultDesc }}</span>
               </el-form-item>
             </el-col>
-            <el-col :span="8">
+            <el-col :span="8"> 
               <el-form-item label="访问时间：">
                 <span>{{ form.createTime }}</span>
               </el-form-item>
